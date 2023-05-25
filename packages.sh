16c4fadc26918389443f9297260d04aeae7aa1bc
Zyrolie <zyrvsuarez07@gmail.com>
2023-05-26 04:17:52 +0800

    modified import packages on use case

--- packages.sh
+++ packages.sh
@@ -2,10 +2,13 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt install python
+sudo apt install python3
+sudo apt install python3-pip
 sudo pip install apache-airflow
 sudo pip install apache-airflow[amazon]
 sudo pip install apache-airflow-providers-amazon
 sudo pip install requests
 sudo pip install pandas
-sudo pip install psycopg2
+sudo pip install boto3
+#sudo apt-get install libpq-dev python3-dev
+#sudo pip install psycopg2
