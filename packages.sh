ef838e307d9e55df6f7795dea20d434cc72c6565
Zyrolie <zyrvsuarez07@gmail.com>
2023-05-29 04:10:50 +0800

    modify version because of conflicting botocore and boto3 versions

--- packages.sh
+++ packages.sh
@@ -9,6 +9,9 @@ sudo pip install apache-airflow[amazon]
 sudo pip install apache-airflow-providers-amazon
 sudo pip install requests
 sudo pip install pandas
-sudo pip install boto3
+sudo pip install boto3 s3fs
+sudo pip install botocore==1.29.102
+sudo pip install boto3==1.26.63
+
 #sudo apt-get install libpq-dev python3-dev
 #sudo pip install psycopg2
